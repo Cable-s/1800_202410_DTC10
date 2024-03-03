a5f6bea36cd8c4ff4e44d5019c28f10d470b68db
YEALI KIM <YKIM229@MY.BCIT.CA>
2024-03-03 10:45:50 -0800

    Add date-time js in date.js for future use

--- scripts/date.js
+++ scripts/date.js
@@ -2,3 +2,9 @@ let date = new Date().toLocaleDateString();
 console.log(date);
 
 document.getElementById("date-goes-here").innerHTML = date;
+
+
+let dateTime = new Date().toLocaleString();
+console.log(dateTime);
+
+// document.getElementById("date-time-goes-here").innerHTML = dateTime;
