9e2068f4b040f24eef1a5781acbc1773eebe21e9
Michael <mcbride1987@gmail.com>
2024-03-15 22:26:41 -0700

    Minor changes to priority view JS to remove unused code portions

--- scripts/priorityView.js
+++ scripts/priorityView.js
@@ -4,7 +4,7 @@ console.log(tasks);
 
 console.log(tasks.length);
 
-function createHightask () {
+function priorityTasks () {
     for (let i = 0; i <= tasks.length; i++) {
         if (tasks[i].importance === 'high'){
             document.getElementById('high-tasks').innerHTML +=
@@ -67,54 +67,5 @@ function createHightask () {
 }
 }
 
-// function createMediumtask () {
-//     for (let i = 1; i <= tasks.length; i++) {
-//         if (tasks[i].importance === 'medium'){
-//             document.getElementById('medium-tasks').innerHTML +=
-//             `
-//         <div class = "bg-medium" style="display:flex; flex-direction:column; margin: 5px 15px; padding: 10px;">
-//             <div style="display:flex; place-content:space-between"> 
-//                 <div>   
-//                     <p style="font-weight:bold">`+tasks[i].title+`
-//                 </div>
-//                 <div style="display:flex">
-//                     <p>`+tasks[i].startTime+`
-//                     <p>-`+tasks[i].endTime+`
-//                 </div>
-//             </div>
-//             <div>
-//                 <p>`+tasks[i].description+`
-//             </div> 
-//         </div >
-//         `;
-//         }
-
-// }
-// }
-
-// function createLowtask () {
-//     for (let i = 1; i <= tasks.length; i++) {
-//         if (tasks[i].importance === 'low'){
-//             document.getElementById('low-tasks').innerHTML +=
-//             `
-//         <div class = "bg-low" style="display:flex; flex-direction:column; margin: 5px 15px; padding: 10px;">
-//             <div style="display:flex; place-content:space-between"> 
-//                 <div>   
-//                     <p style="font-weight:bold">`+tasks[i].title+`
-//                 </div>
-//                 <div style="display:flex">
-//                     <p>`+tasks[i].startTime+`
-//                     <p>-`+tasks[i].endTime+`
-//                 </div>
-//             </div>
-//             <div>
-//                 <p>`+tasks[i].description+`
-//             </div> 
-//         </div >
-//         `;
-//         }
-
-// }
-// }
-createHightask()
+priorityTasks()
 
