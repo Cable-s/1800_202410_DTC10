cb12ec04b68eda4f888f01c5b63ff09c7054b0b6
Caleb <caleb.s.janzen@gmail.com>
2024-03-23 18:02:07 -0700

    add expired date modal and logic

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -4,12 +4,11 @@
 // This function loads the parts of your skeleton 
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
-function load(url, element)
-{
+function load(url, element) {
     // modified code from Stack Overflow question:
     // https://stackoverflow.com/questions/17901116/i-need-the-equivalent-of-load-to-js
     fetch(url)
-        .then( response => response.text())
+        .then(response => response.text())
         .then(html => {
             element.innerHTML = html;
         })
@@ -21,7 +20,7 @@ function loadSkeleton() {
         if (user) {                   //if the pointer to "user" object is not null, then someone is logged in
             // User is signed in.
             // Do something for the user here.
-            
+
             // Old jQuery code - causes browser to warn about deprecated feature
             // console.log($('#navbarTemplate').load('./text/nav_after_login.html'));
             // console.log($('#footerTemplate').load('./text/footer_after_login.html'));
@@ -29,9 +28,10 @@ function loadSkeleton() {
             load("./text/nav_after_login.html", document.getElementById("navbarTemplate"));
             load("./text/footer_after_login.html", document.getElementById("footerTemplate"));
             load("./text/addTaskModal.html", document.getElementById("addTaskModal"));
+            load("./text/expiredTaskModal.html", document.getElementById("expiredTaskModal"));
         } else {
             // No user is signed in.
-            
+
             // Old jQuery code - causes browser to warn about deprecated feature
             //console.log($('#navbarTemplate').load('./text/nav_before_login.html'));
             //console.log($('#footerTemplate').load('./text/footer.html'));
