7fd5721afeddccf7f50c7d85cb82defb95d8a121
Caleb <caleb.s.janzen@gmail.com>
2024-03-24 18:01:30 -0700

    Fix bug where the task error would show up and still create the task

--- scripts/addTask.js
+++ scripts/addTask.js
@@ -12,10 +12,10 @@ function submitForm() {
         { inputId: 'importanceSelect', errorId: 'importance-error', value: document.getElementById('importanceSelect').value, errorMessage: "Importance is required" }
     ];
 
-    fieldsToValidate.forEach(function(field) {
+    fieldsToValidate.forEach(function (field) {
         // Clear existing error message
         document.getElementById(field.errorId).textContent = '';
-        
+
         // Validate and show error message if necessary
         if (!field.value) {
             showError(field.inputId, field.errorMessage);
@@ -29,23 +29,27 @@ function submitForm() {
     var endDate = new Date(document.getElementById('endDate').value);
     var startTime = document.getElementById('startTime').value;
     var endTime = document.getElementById('endTime').value;
+    var error = false
 
     if (startDate.getTime() > endDate.getTime()) {
+        error = true
         showError('endDate', "End date must be after start date");
     }
 
     if (startDate.getTime() == endDate.getTime() && startTime >= endTime) {
+        error = true
         showError('endTime', "End time must be after start time");
     }
 
     if (endDate.getTime() < startDate.getTime()) {
+        error = true
         showError('endDate', "End date cannot be before start date");
     }
 
     var title = fieldsToValidate[0].value;
     var importance = fieldsToValidate[5].value;
 
-    if (title && startDate && startTime && endDate && endTime && importance) {
+    if (title && startDate && startTime && endDate && endTime && importance && !error) {
         // Proceed with form submission
         const task = db.collection("users").doc(firebase.auth().currentUser.uid).collection("tasks");
         task.add({}).then(docRef => {
