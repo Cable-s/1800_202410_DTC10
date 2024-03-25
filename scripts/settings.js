ba2c1e990ff46efcf16b3897ef14d321774d9710
Travis Gooden <tgooden@my.bcit.ca>
2024-03-24 19:50:16 -0700

    Add submit button instead of live updating settings

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,5 +1,15 @@
+var userID;
+var checked;
+var languageSelect = document.getElementById("languageSelect");
+var timezoneSelect = document.getElementById("timezoneSelect");
+var flexSwitch = document.querySelector(
+  "#flexSwitchCheckChecked.form-check-input",
+);
+var fontSelect = document.getElementById("fontSelect");
+var themeSelect = document.getElementById("themeSelect");
+var submitBtn = document.getElementById("submitBtn");
+
 function query() {
-  var userID;
   var data;
   return new Promise((resolve, reject) => {
     firebase.auth().onAuthStateChanged((user) => {
@@ -21,11 +31,46 @@ function query() {
     });
   });
 }
-let userSettings = await query().then((doc) => {
-  let result = doc.data();
-  localStorage.setItem("language", result.language);
-  localStorage.setItem("fontSelect", result.fontSelect);
-  localStorage.setItem("notifications", result.notifications);
-  localStorage.setItem("themeSelect", result.themeSelect);
-  localStorage.setItem("timezoneSelect", result.timezoneSelect);
-});
+async function userSettings() {
+  let settings = await query().then((doc) => {
+    let result = doc.data();
+    localStorage.setItem("language", result.language);
+    localStorage.setItem("fontSelect", result.fontSelect);
+    localStorage.setItem("notifications", result.notifications);
+    localStorage.setItem("themeSelect", result.themeSelect);
+    localStorage.setItem("timezoneSelect", result.timezoneSelect);
+
+    languageSelect.value = localStorage.getItem("language");
+    timezoneSelect.value = localStorage.getItem("timezoneSelect");
+    themeSelect.value = localStorage.getItem("themeSelect");
+    fontSelect.value = localStorage.getItem("fontSelect");
+  });
+}
+function updateSettings(newSetting) {
+  Object.entries(newSetting).forEach(([key, value]) => {
+    db.collection("users")
+      .doc(`${userID}`)
+      .collection("settings")
+      .doc(`${localStorage.getItem("settingsID")}`)
+      .update({ [key]: value })
+      .then(() => {
+        console.log("Document successfully written!");
+      })
+      .catch((error) => {
+        console.error("Error writing document: ", error);
+      });
+  });
+  userSettings();
+}
+function addListener() {
+  submitBtn.addEventListener("submit", () => {
+    updateSettings({
+      language: languageSelect.value,
+      fontSelect: fontSelect.value,
+      themeSelect: themeSelect.value,
+      notifications: flexSwitch.value,
+    });
+  });
+}
+addListeners();
+userSettings();
