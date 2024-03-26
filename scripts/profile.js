4df21f0a09a09efbca7a5e40aaeb88eb827f2b39
Travis Gooden <tgooden@my.bcit.ca>
2024-03-25 21:44:08 -0700

    Read and update profile information from firestore

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,4 +1,12 @@
-var docId;
+var userID;
+var docID;
+var accountDate;
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const about = document.getElementById("about");
+const birthday = document.getElementById("birthday");
+const submit = document.getElementById("submit");
+const joinDate = document.getElementById("joinDate");
 
 function query() {
   var data;
@@ -6,6 +14,7 @@ function query() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         userID = user.uid;
+        accountDate = user.metadata.creationTime;
         // User is signed in.
         data = db
           .collection("users")
@@ -14,6 +23,7 @@ function query() {
           .get()
           .then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
+              docID = doc.id;
               resolve(doc.id);
             });
           });
@@ -21,11 +31,44 @@ function query() {
     });
   });
 }
+
+function loadProfile() {
+  db.collection("users")
+    .doc(`${userID}`)
+    .collection("profile")
+    .doc(`${docID}`)
+    .get()
+    .then((doc) => {
+      let result = doc.data();
+      firstName.value = result.firstName;
+      lastName.value = result.lastName;
+      about.value = result.about;
+      birthday.value = result.birthday;
+      joinDate.innerText = "Member since " + accountDate;
+    });
+}
 async function queryProfile() {
   let profile = await query();
-  docId = profile;
+  return profile;
 }
 
-queryProfile();
+queryProfile().then((doc) => {
+  loadProfile();
+});
 
-console.log(docId);
+submit.addEventListener("click", () => {
+  db.collection("users")
+    .doc(`${userID}`)
+    .collection("profile")
+    .doc(`${docID}`)
+    .update({
+      firstName: `${firstName.value}`,
+      lastName: `${lastName.value}`,
+      about: `${about.value}`,
+      birthday: `${birthday.value}`,
+    })
+    .then(console.log("Document written"))
+    .catch((error) => {
+      console.log(error);
+    });
+});
