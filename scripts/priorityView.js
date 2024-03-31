3bf15c3659c9a72aa6b4d6eb966c921bbdf62b1d
Michael <mcbride1987@gmail.com>
2024-03-31 16:08:08 -0700

    Fix function call from displayTasksByDate to priorityTasks to correct error.

--- scripts/priorityView.js
+++ scripts/priorityView.js
@@ -65,7 +65,7 @@ function setDefaultDate() {
   let today = new Date()
   today = today.getFullYear() + "-" + zeroPad((today.getMonth() + 1), 2) + "-" + today.getDate()
   document.getElementById('selectedDate').value = today;
-  displayTasksByDate(today);
+  priorityTasks(today);
 }
 
 // Set today's date as the default value
@@ -74,7 +74,7 @@ setDefaultDate();
 // Add event listener to update displayed date when date input changes
 document.getElementById('selectedDate').addEventListener('input', function () {
   clearTasks()
-  displayTasksByDate(this.value);
+  priorityTasks(this.value);
 });
 
 // Initial display of tasks based on today's date
