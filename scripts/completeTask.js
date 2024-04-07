98feadc6b29fc7da836c362ce1454c8fce23c15c
Travis Gooden <tgooden@my.bcit.ca>
2024-04-06 22:25:04 -0700

    Remove authentication script from index.html

--- scripts/completeTask.js
+++ scripts/completeTask.js
@@ -1,11 +1,11 @@
 import { query } from "./queryDocuments.js";
 let tasks = await query("tasks");
-
+var userId = sessionStorage.getItem("userId");
 function complete(id) {
   for (let i = 0; i < tasks.length; i++) {
     if (id == tasks[i].id) {
       db.collection("users")
-        .doc(firebase.auth().currentUser.uid)
+        .doc(userId)
         .collection("tasks")
         .doc(id)
         .delete()
@@ -16,4 +16,3 @@ function complete(id) {
   }
 }
 window.complete = complete;
-
